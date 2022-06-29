b04bd8d7668aa0583abcb9b6bae78f9f7486094a
Haxorsprogramming <haxors@uinsu.ac.id>
2022-06-29 22:48:29 +0700

    update

---
+++ public/base/js/login.js
@@ -0,0 +1,15 @@
+// vue object 
+var appLogin = new Vue({
+    el : '#divLogin',
+    data : {
+
+    },
+    methods : {
+        loginAtc : function()
+        {
+            let username = document.querySelector("#txtUsername").value;
+            let password = document.querySelector("#txtPassword").value;
+            
+        }
+    }
+});
